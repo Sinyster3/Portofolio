8069f592a6cd09f8c451ef6a0ab33c0841027db9
Habli <mhabliislamy28@gmail.com>
2026-07-29 13:21:56 +0700

    Fix GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,11 @@ import App from "./App";
 import "./index.css";
 import { HashRouter } from "react-router-dom";
 
-<HashRouter>
-    <App />
-</HashRouter>
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
